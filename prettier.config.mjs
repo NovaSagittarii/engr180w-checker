3a6f4f45c270d8c78da8ade61cb8f6020603be65
NovaSagittarii <novasagittarii@gmail.com>
2024-01-11 18:25:25 -0700

    vite setup, tailwind, prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: true,
+  trailingComma: "all",
+  arrowParens: "always",
+  proseWrap: "preserve",
+};
+
+export default config;
