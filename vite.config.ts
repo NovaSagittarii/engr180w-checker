15b1deff6e30e376cd3f6fc661b5bbcaca1d2cb7
NovaSagittarii <novasagittarii@gmail.com>
2024-01-17 14:23:18 -0700

    add vite node polyfills to support the syllable count module

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/engr180w-checker/",
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
 });
