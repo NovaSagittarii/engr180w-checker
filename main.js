03cd84ef27f8ceeb73286957a0976d35f9c4b98f
NovaSagittarii <thomas1210@gmail.com>
2024-01-18 22:53:02 +0000

    Deploying to gh-pages from @ NovaSagittarii/engr180w-checker@a927ba182ea862904a1d1d095c9d7f639a7febc4 🚀

---
+++ main.js
@@ -0,0 +1,293 @@
+"use strict";
+(self["webpackChunkengr180w_checker"] = self["webpackChunkengr180w_checker"] || []).push([["main"],{
+
+/***/ 383:
+/*!*******************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./node_modules/postcss-loader/dist/cjs.js!./src/index.css ***!
+  \*******************************************************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/sourceMaps.js */ 537);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ 645);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, `@tailwind base;
+@tailwind components;
+@tailwind utilities;
+`, "",{"version":3,"sources":["webpack://./src/index.css"],"names":[],"mappings":"AAAA,cAAc;AACd,oBAAoB;AACpB,mBAAmB","sourcesContent":["@tailwind base;\n@tailwind components;\n@tailwind utilities;\n"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ 722:
+/*!***********************!*\
+  !*** ./src/index.css ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ 379);
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_style_loader_dist_runtime_styleDomAPI_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/styleDomAPI.js */ 795);
+/* harmony import */ var _node_modules_style_loader_dist_runtime_styleDomAPI_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_styleDomAPI_js__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _node_modules_style_loader_dist_runtime_insertBySelector_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/insertBySelector.js */ 569);
+/* harmony import */ var _node_modules_style_loader_dist_runtime_insertBySelector_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_insertBySelector_js__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _node_modules_style_loader_dist_runtime_setAttributesWithoutAttributes_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/setAttributesWithoutAttributes.js */ 565);
+/* harmony import */ var _node_modules_style_loader_dist_runtime_setAttributesWithoutAttributes_js__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_setAttributesWithoutAttributes_js__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _node_modules_style_loader_dist_runtime_insertStyleElement_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/insertStyleElement.js */ 216);
+/* harmony import */ var _node_modules_style_loader_dist_runtime_insertStyleElement_js__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_insertStyleElement_js__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var _node_modules_style_loader_dist_runtime_styleTagTransform_js__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/styleTagTransform.js */ 589);
+/* harmony import */ var _node_modules_style_loader_dist_runtime_styleTagTransform_js__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_styleTagTransform_js__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_node_modules_postcss_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! !!../node_modules/css-loader/dist/cjs.js!../node_modules/postcss-loader/dist/cjs.js!./index.css */ 383);
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+
+var options = {};
+
+options.styleTagTransform = (_node_modules_style_loader_dist_runtime_styleTagTransform_js__WEBPACK_IMPORTED_MODULE_5___default());
+options.setAttributes = (_node_modules_style_loader_dist_runtime_setAttributesWithoutAttributes_js__WEBPACK_IMPORTED_MODULE_3___default());
+
+      options.insert = _node_modules_style_loader_dist_runtime_insertBySelector_js__WEBPACK_IMPORTED_MODULE_2___default().bind(null, "head");
+    
+options.domAPI = (_node_modules_style_loader_dist_runtime_styleDomAPI_js__WEBPACK_IMPORTED_MODULE_1___default());
+options.insertStyleElement = (_node_modules_style_loader_dist_runtime_insertStyleElement_js__WEBPACK_IMPORTED_MODULE_4___default());
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_node_modules_postcss_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_6__["default"], options);
+
+
+
+
+       /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_node_modules_postcss_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_6__["default"] && _node_modules_css_loader_dist_cjs_js_node_modules_postcss_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_6__["default"].locals ? _node_modules_css_loader_dist_cjs_js_node_modules_postcss_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_6__["default"].locals : undefined);
+
+
+/***/ }),
+
+/***/ 162:
+/*!*******************************!*\
+  !*** ./src/AnnotatedText.tsx ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ 294);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var write_good__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! write-good */ 859);
+/* harmony import */ var write_good__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(write_good__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _Issue__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Issue */ 428);
+
+
+
+function AnnotatedText({ text }) {
+    const WhitespaceRegex = /\s/g;
+    const [mouseIndex, setMouseIndex] = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(-1);
+    const [suggestions, setSuggestions] = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)([]);
+    const [issueCount, setIssueCount] = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)([]);
+    (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(() => {
+        const suggestions = write_good__WEBPACK_IMPORTED_MODULE_1___default()(text);
+        function addSuggestion(index, offset, reason) {
+            suggestions.push({
+                index,
+                offset,
+                reason,
+            });
+        }
+        const paragraphRegex = /\n[^\n]+|[^\n]+\n/gi;
+        let match;
+        while ((match = paragraphRegex.exec(text))) {
+            const paragraph = match[0];
+            const paragraphStart = match.index;
+            // suggestions.push({
+            //   index: match.index,
+            //   offset: match[0].length,
+            //   reason: "this is a paragraph",
+            // });
+            const sentenceRegex = /[^.]+./gi;
+            while ((match = sentenceRegex.exec(paragraph))) {
+                const sentence = match[0];
+                const sentenceStart = match.index + paragraphStart;
+                const sentenceOffset = sentence.length;
+                console.log(sentence, sentence.split(" ").length);
+                if (sentence.split(" ").length >= 20) {
+                    addSuggestion(sentenceStart, sentenceOffset, "Long sentence!");
+                }
+                if (sentence.split(" ").length >= 25) {
+                    addSuggestion(sentenceStart, sentenceOffset, "Very long sentence!");
+                }
+            }
+        }
+        const issueCount = [...new Array(text.length)].map(() => 0);
+        for (const { index, offset } of suggestions) {
+            for (let i = 0; i < offset; ++i) {
+                issueCount[index + i] += 1;
+            }
+        }
+        setSuggestions(suggestions);
+        setIssueCount(issueCount);
+    }, [text]);
+    function issueCountToColor(issues) {
+        if (issues >= 5)
+            return "text-red-500";
+        else {
+            return [
+                "",
+                "text-yellow-500",
+                "text-yellow-800",
+                "text-orange-500",
+                "text-orange-800",
+            ][issues];
+        }
+    }
+    return (react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { className: 'flex flex-row w-full max-w-md flex-wrap font-mono' },
+        text.split("").map((c, index) => (react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { key: index, className: `${WhitespaceRegex.test(c) ? "px-2" : ""} ${issueCountToColor(issueCount[index])} ${index === mouseIndex ? "bg-slate-500" : ""}`, onMouseOver: () => setMouseIndex(index) }, c))),
+        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { className: 'fixed bg-white p-2 border border-black rounded-md text-black' }, suggestions.map(({ reason, index, offset }, suggestionIndex) => mouseIndex >= index &&
+            mouseIndex <= index + offset && (react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_Issue__WEBPACK_IMPORTED_MODULE_2__.Issue, { label: '!', description: reason, key: suggestionIndex }))))));
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (AnnotatedText);
+
+
+/***/ }),
+
+/***/ 991:
+/*!*********************!*\
+  !*** ./src/App.tsx ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ 294);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _Editor__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Editor */ 481);
+
+
+function App() {
+    return (react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null,
+        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, "Paste text"),
+        react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_Editor__WEBPACK_IMPORTED_MODULE_1__["default"], null)));
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (App);
+
+
+/***/ }),
+
+/***/ 481:
+/*!************************!*\
+  !*** ./src/Editor.tsx ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ 294);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _AnnotatedText__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./AnnotatedText */ 162);
+
+
+function Editor() {
+    const [content, setContent] = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)("The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.\n");
+    return (react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { className: 'm-4 flex flex-col items-center' },
+        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("textarea", { onChange: (event) => setContent(event.target.value), placeholder: "add text to check", className: 'w-full border border-black' }),
+        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { className: 'p-4 m-4 rounded-lg bg-white border border-black/20 max-w-screen-md' },
+            react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_AnnotatedText__WEBPACK_IMPORTED_MODULE_1__["default"], { text: content }))));
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Editor);
+
+
+/***/ }),
+
+/***/ 428:
+/*!***********************!*\
+  !*** ./src/Issue.tsx ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   Issue: () => (/* binding */ Issue),
+/* harmony export */   IssueListing: () => (/* binding */ IssueListing),
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ 294);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+const BG_COLORS = {
+    0: "bg-yellow-100",
+    1: "bg-orange-100",
+    2: "bg-red-100",
+    3: "bg-red-100",
+};
+function Issue({ label, description, severity = 0, key }) {
+    const [active, _setActive] = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(true);
+    return (react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { className: `flex flex-col p-1 rounded-sm ${active ? "" : "opacity-50"}`, key: key },
+        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { className: `font-semibold text-xl ${BG_COLORS[severity]}` }, label),
+        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null,
+            react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", { className: 'text-black/80 text-base' }, description))));
+}
+function IssueListing({ issues }) {
+    return (react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, issues.getIssues().map(({ label, description, severity }, index) => (react__WEBPACK_IMPORTED_MODULE_0___default().createElement(Issue, { label: label, description: description, key: index, severity: severity })))));
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Issue);
+
+
+
+/***/ }),
+
+/***/ 472:
+/*!**********************!*\
+  !*** ./src/main.tsx ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ 294);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_dom_client__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom/client */ 745);
+/* harmony import */ var _App__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./App */ 991);
+/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./index.css */ 722);
+
+
+
+
+react_dom_client__WEBPACK_IMPORTED_MODULE_1__.createRoot(document.getElementById("root")).render(react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().StrictMode), null,
+    react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_App__WEBPACK_IMPORTED_MODULE_2__["default"], null)));
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["vendors"], () => (__webpack_exec__(472)));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
+//# sourceMappingURL=main.js.map
